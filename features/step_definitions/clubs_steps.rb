a71c81a7feb322eb17ac30b30dec639fc5c6e529
bryce-klinker <bklinker@pillartechnology.com>
2016-08-09 12:09:19 -0500

    Added all club names to expected list

--- features/step_definitions/clubs_steps.rb
+++ features/step_definitions/clubs_steps.rb
@@ -10,10 +10,30 @@ When(/^I get all clubs$/) do
 end
 
 Then(/^I should get Premier League clubs for the 2015\/2016 season$/) do
-  expect(@clubs.size).to equal(20)
+  expect(@clubs.size).to equal 20
 end
 
 Then(/^I should get each club's name$/) do
   clubNames = @clubs.collect { |c| c.name }
-  expect(clubNames).to include('Arsenal')
+
+  expect(clubNames).to include 'Arsenal'
+  expect(clubNames).to include 'Aston Villa'
+  expect(clubNames).to include 'Bournemouth'
+  expect(clubNames).to include 'Chelsea'
+  expect(clubNames).to include 'Everton'
+  expect(clubNames).to include 'Crystal Palace'
+  expect(clubNames).to include 'Man United'
+  expect(clubNames).to include 'Swansea'
+  expect(clubNames).to include 'Watford'
+  expect(clubNames).to include 'Leicester'
+  expect(clubNames).to include 'Sunderland'
+  expect(clubNames).to include 'Tottenham'
+  expect(clubNames).to include 'West Ham'
+  expect(clubNames).to include 'Newcastle'
+  expect(clubNames).to include 'West Brom'
+  expect(clubNames).to include 'Man City'
+  expect(clubNames).to include 'Southampton'
+  expect(clubNames).to include 'Liverpool'
+  expect(clubNames).to include 'Norwich'
+  expect(clubNames).to include 'Stoke'
 end

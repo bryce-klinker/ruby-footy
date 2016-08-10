415f2680d9c87b1d6d931038fba1608de31b910e
bryce-klinker <bklinker@pillartechnology.com>
2016-08-10 09:45:54 -0500

    Added test for season leader board

--- features/step_definitions/seasons_steps.rb
+++ features/step_definitions/seasons_steps.rb
@@ -13,4 +13,23 @@ end
 
 Then(/^I should have leader board for 2015_2016 Premier League season$/) do
   expect(@season.leader_board[0].name).to eql 'Leicester City'
+  expect(@season.leader_board[1].name).to eql 'Arsenal'
+  expect(@season.leader_board[2].name).to eql 'Tottenham'
+  expect(@season.leader_board[3].name).to eql 'Man City'
+  expect(@season.leader_board[4].name).to eql 'Man United'
+  expect(@season.leader_board[5].name).to eql 'Southampton'
+  expect(@season.leader_board[6].name).to eql 'West Ham'
+  expect(@season.leader_board[7].name).to eql 'Liverpool'
+  expect(@season.leader_board[8].name).to eql 'Stoke'
+  expect(@season.leader_board[9].name).to eql 'Chelsea'
+  expect(@season.leader_board[10].name).to eql 'Everton'
+  expect(@season.leader_board[11].name).to eql 'Swansea'
+  expect(@season.leader_board[12].name).to eql 'Watford'
+  expect(@season.leader_board[13].name).to eql 'West Brom'
+  expect(@season.leader_board[14].name).to eql 'Crystal Palace'
+  expect(@season.leader_board[15].name).to eql 'Bournemouth'
+  expect(@season.leader_board[16].name).to eql 'Sunderland'
+  expect(@season.leader_board[17].name).to eql 'Newcastle'
+  expect(@season.leader_board[18].name).to eql 'Norwich'
+  expect(@season.leader_board[19].name).to eql 'Aston Villa'
 end

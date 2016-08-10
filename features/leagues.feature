cb7966321613aac7acca59e439ce5dbad5ebfccc
bryce-klinker <bklinker@pillartechnology.com>
2016-08-10 11:55:46 -0500

    Change season.feature to seasons.feature. Added leagues feature to get league specific data

---
+++ features/leagues.feature
@@ -0,0 +1,8 @@
+Feature: Leagues
+  As a football fan
+  I want to have leagues
+  So that I can track my club across leagues
+
+  Scenario: Get Leagues
+    When I get leagues
+    Then I should get Premier League
---
+++ features/step_definitions/leagues_steps.rb
@@ -0,0 +1,13 @@
+require './src/leagues/league_gateway'
+
+When(/^I get leagues$/) do
+  seasons_path = File.expand_path './data/seasons'
+  league_gateway = LeagueGateway.new seasons_path
+  @leagues = league_gateway.get_all
+end
+
+
+Then(/^I should get (.*)$/) do |league_name|
+  league_names = @leagues.map{ |l| l.name }
+  expect(league_names).to include league_name
+end

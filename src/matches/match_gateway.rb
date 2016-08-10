bbde3a9e9ff6116a76dac9cffd80478f48f62c4c
bryce-klinker <bklinker@pillartechnology.com>
2016-08-10 11:23:43 -0500

    Changed match gateway to cache results after first read

--- src/matches/match_gateway.rb
+++ src/matches/match_gateway.rb
@@ -1,13 +1,21 @@
 require_relative 'match.rb'
 
 class MatchGateway
+
+
   def initialize(csv_path)
     @csv_path = csv_path
   end
 
   def get_all
-    File.readlines(@csv_path)
+    if @matches
+      return @matches
+    end
+    
+    @matches = File.readlines(@csv_path)
         .drop(1)
         .map{ |line| Match.new(line) }
+
+    @matches
   end
 end

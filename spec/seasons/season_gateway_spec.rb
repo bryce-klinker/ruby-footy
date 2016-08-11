141941d1523653e7267894a32071b8d5af037078
bryce-klinker <bklinker@pillartechnology.com>
2016-08-11 08:59:35 -0500

    Adding test for new method on gateway to get a single season

--- spec/seasons/season_gateway_spec.rb
+++ spec/seasons/season_gateway_spec.rb
@@ -27,4 +27,11 @@ describe 'SeasonGateway' do
     seasons = @season_gateway.get_by_league_name 'Premier League'
     expect(seasons[0].league_name).to eql 'Premier League'
   end
+
+  it 'should get season for start year, end year, and league name' do
+    season = @season_gateway.get_season '2015', '2016', 'Premier League'
+    expect(season.start_year).to eql 2015
+    expect(season.end_year).to eql 2016
+    expect(season.league_name).to eql 'Premier League'
+  end
 end

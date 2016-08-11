d3f1fc11485e3baa8f5e9ca3930fe917c46381ba
bryce-klinker <bklinker@pillartechnology.com>
2016-08-11 09:57:10 -0500

    Fixed test to ensure content type is json

--- src/footy_app.rb
+++ src/footy_app.rb
@@ -5,7 +5,7 @@ require_relative 'shared/footy_config'
 
 season_gateway = SeasonGateway.new FootyConfig.seasons_directory
 get '/seasons/:start_year/:end_year/:league_name/clubs' do
-  # content_type :json
+  content_type :json
   season = season_gateway.get_season(params['start_year'], params['end_year'], params['league_name'])
   season.clubs.map{ |c| c.to_json }.to_json
 end

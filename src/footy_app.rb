127822f4b4d76acbebb745222cef350f6aaa2dac
bryce-klinker <bklinker@pillartechnology.com>
2016-08-11 09:54:34 -0500

    Removing unneeded code from seasons get

--- src/footy_app.rb
+++ src/footy_app.rb
@@ -5,12 +5,7 @@ require_relative 'shared/footy_config'
 
 season_gateway = SeasonGateway.new FootyConfig.seasons_directory
 get '/seasons/:start_year/:end_year/:league_name/clubs' do
-  content_type :json
-  start_year = params['start_year']
-  end_year = params['end_year']
-  league_name = params['league_name']
-  season = season_gateway.get_season(start_year, end_year, league_name)
-  clubs = season.clubs
-  json = clubs.map{ |c| c.to_json }
-  json.to_json
+  # content_type :json
+  season = season_gateway.get_season(params['start_year'], params['end_year'], params['league_name'])
+  season.clubs.map{ |c| c.to_json }.to_json
 end

e0dfdd171ce6b9cf32a40ceacdccd424c613b9eb
bryce-klinker <bklinker@pillartechnology.com>
2016-08-11 08:46:28 -0500

    Added test that checks the body of the response for data

--- spec/footy_app_spec.rb
+++ spec/footy_app_spec.rb
@@ -1,6 +1,14 @@
+require 'multi_json'
+
 describe 'FootyApp' do
-  it 'should get clubs for season' do
+  it 'should respond successfully to requests for clubs' do
     get '/seasons/2015/2016/Premier_League/clubs'
     expect(last_response.status).to eql 200
+  end
+
+  it 'should return all clubs for requested season' do
+    get '/seasons/2014/2015/Premier_League/clubs'
+    clubs = MultiJson.load(last_response.body)
+    expect(clubs.length).to be 20
   end
 end

849559957bec076be1f3b5f7c64d54600d7b1169
bryce-klinker <bklinker@pillartechnology.com>
2016-08-11 10:24:41 -0500

    Added fix for getting leagues from api

--- spec/footy_app_spec.rb
+++ spec/footy_app_spec.rb
@@ -8,12 +8,23 @@ describe 'FootyApp' do
 
   it 'should return all clubs for requested season' do
     get '/seasons/2014/2015/Premier_League/clubs'
-    clubs = JSON.parse(last_response.body)
+    clubs = JSON.parse last_response.body
     expect(clubs.length).to be 20
   end
 
   it 'should set content type to json for getting clubs for a season' do
     get '/seasons/2015/2016/Premier_League/clubs'
     expect(last_response.headers['content-type']).to eql 'application/json'
+  end
+
+  it 'should respond successfully to requests for leagues' do
+    get '/leagues'
+    expect(last_response.status).to eql 200
+  end
+
+  it 'should return all leagues for request' do
+    get '/leagues'
+    leagues = JSON.parse last_response.body
+    expect(leagues.length).to eql 2
   end
 end
--- src/footy_app.rb
+++ src/footy_app.rb
@@ -1,11 +1,17 @@
 require 'sinatra'
 require 'json'
 require_relative 'seasons/season_gateway'
+require_relative 'leagues/league_gateway'
 require_relative 'shared/footy_config'
 
 season_gateway = SeasonGateway.new FootyConfig.seasons_directory
+league_gateway = LeagueGateway.new FootyConfig.seasons_directory
 get '/seasons/:start_year/:end_year/:league_name/clubs' do
   content_type :json
   season = season_gateway.get_season(params['start_year'], params['end_year'], params['league_name'])
   season.clubs.map{ |c| c.to_json }.to_json
+end
+
+get '/leagues' do
+  league_gateway.get_all.map{ |l| l.to_json }.to_json
 end
